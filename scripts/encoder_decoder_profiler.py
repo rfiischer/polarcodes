a5fb92c0d21b939673a9b3a7d450918bfccd8073
Rodrigo Fischer <rodrfischer@hotmail.com>
2020-02-17 17:51:18 -0300

    [En/decoder profiler]

---
+++ scripts/encoder_decoder_profiler.py
@@ -0,0 +1,22 @@
+"""
+
+
+Created on 17/02/2020 17:33
+
+@author: Rodrigo Fischer (rodrigoarfischer@gmail.com)
+"""
+
+import numpy as np
+
+from polarcoding import PolarCoding
+
+n = 10
+frames = 1000
+pc = PolarCoding(n)
+
+bits = np.random.randint(0, 2, 2 ** n)
+
+for frame in range(0, frames):
+    encoded = pc.encode(bits)
+    llr = [1 if value == 0 else -1 for value in encoded]
+    decoded = pc.decode(llr)

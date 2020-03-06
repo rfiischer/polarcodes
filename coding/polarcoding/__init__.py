712d1043103247315db5b6e18b88794145d2eb3a
Rodrigo Fischer <rodrfischer@hotmail.com>
2020-03-06 17:26:28 -0300

    [Further structural changes]

---
+++ coding/polarcoding/__init__.py
@@ -0,0 +1,9 @@
+"""
+
+
+Created on 06/03/2020 16:51
+
+@author: Rodrigo Fischer (rodrigoarfischer@gmail.com)
+"""
+
+from coding.polarcoding.polarcoding import PolarCoding
--- coding/polarcoding/polarcoding.py
+++ coding/polarcoding/polarcoding.py
@@ -7,49 +7,8 @@ Created on 12/02/2020 11:25
 """
 
 import numpy as np
-import numba as nb
 
-
-@nb.njit("float64(float64, float64)")
-def _fl(a, b):
-    return np.log((1 + np.exp(a + b)) / (np.exp(a) + np.exp(b)))
-
-
-@nb.njit("float64(float64, float64, float64)")
-def _fr(a, b, c):
-    return b + (1 - 2 * c) * a
-
-
-@nb.njit("float64[:](float64[:], uint8[:])")
-def _alpha_right(alphas, betas):
-    out_size = alphas.size // 2
-    alphas_out = np.zeros(out_size)
-    for i in nb.prange(0, out_size):
-        alphas_out[i] = _fr(alphas[i], alphas[i + out_size], betas[i])
-
-    return alphas_out
-
-
-@nb.njit("float64[:](float64[:])")
-def _alpha_left(alphas):
-    out_size = alphas.size // 2
-    alphas_out = np.zeros(out_size)
-    for i in range(0, out_size):
-        alphas_out[i] = _fl(alphas[i], alphas[i + out_size])
-
-    return alphas_out
-
-
-@nb.njit("uint8[:](uint8[:], uint8[:])")
-def _betas(betas_left, betas_right):
-    betas_size = betas_left.size
-    out_size = 2 * betas_size
-    betas_out = np.zeros(out_size, dtype=np.uint8)
-    for i in nb.prange(0, betas_size):
-        betas_out[i] = betas_left[i] ^ betas_right[i]
-        betas_out[i + betas_size] = betas_right[i]
-
-    return betas_out
+from coding.polarcoding.polarfuncs import *
 
 
 class PolarCoding(object):

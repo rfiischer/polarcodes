5cc1f81b090578d999238fb3258c9680136ccc35
Rodrigo Fischer <rodrfischer@hotmail.com>
2020-03-09 14:24:31 -0300

    [Automate compilation]

--- coding/polarcoding/polarfuncs/__init__.py
+++ coding/polarcoding/polarfuncs/__init__.py
@@ -6,4 +6,11 @@ Created on 06/03/2020 17:35
 @author: Rodrigo Fischer (rodrigoarfischer@gmail.com)
 """
 
-from coding.polarcoding.polarfuncs.polarfuncs import alpha_left, alpha_right, betas, fr, fl, compute_node
+import logging
+
+try:
+    from coding.polarcoding.polarfuncs.polarfuncs_compiled import alpha_left, alpha_right, betas, fr, fl, compute_node
+
+except ImportError:
+    from coding.polarcoding.polarfuncs.polarfuncs import alpha_left, alpha_right, betas, fr, fl, compute_node
+    logging.warning("Using pure Python implementation. To use Pythran, run the compile script.")
---
+++ coding/polarcoding/polarfuncs/compile.py
@@ -0,0 +1,11 @@
+"""
+Compile the Pythran optimization
+
+Created on 09/03/2020 14:19
+
+@author: Rodrigo Fischer (rodrigoarfischer@gmail.com)
+"""
+
+import pythran
+
+pythran.compile_pythranfile("polarfuncs.py", output_file="polarfuncs_compiled%{ext}")

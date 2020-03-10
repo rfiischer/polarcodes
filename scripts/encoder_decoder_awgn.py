1ffb51dfca4bc410ae81b2834817e7e7827be817
Rodrigo Fischer <rodrfischer@hotmail.com>
2020-03-10 14:19:10 -0300

    [Bit error campaign]

--- scripts/encoder_decoder_awgn.py
+++ scripts/encoder_decoder_awgn.py
@@ -15,23 +15,25 @@ from core.mod_demod import Modulator, Demodulator
 
 n = 4
 k = 1
-frames = 100000
+frames = 1000000
 snr = 1
 rng = np.random.RandomState(seed=12465)
-pc = PolarCoding(n, rel_idx=[7, 0, 1, 2, 3, 4, 5, 6, 8, 13, 9, 10, 11, 12, 14, 15])
 awgn = AWGN(1, rng, snr_unit="EsN0_dB")
 constellation = PolarConstellation()
 mod = Modulator(constellation, 2)
 dem = Demodulator(constellation, 'llr_exact', 2)
 
-err_sum = 0
-for frame in range(0, frames):
-    bits = np.random.randint(0, 2, k, dtype=np.uint8)
-    encoded = pc.encode(bits)
-    modulated = mod(encoded)
-    noise_symbols = awgn(modulated, snr=snr)
-    llr = dem(noise_symbols, variance=awgn.variance)
-    decoded = pc.decode(llr)
-    err_sum += np.sum(np.bitwise_xor(bits, decoded))
+err_sum = np.zeros(2 ** n)
+for i in range(0, 2 ** n):
+    for frame in range(0, frames):
+        rel = np.concatenate(([i], np.delete(np.arange(0, 16), i)))
+        pc = PolarCoding(n, rel_idx=rel)
+        bits = np.random.randint(0, 2, k, dtype=np.uint8)
+        encoded = pc.encode(bits)
+        modulated = mod(encoded)
+        noise_symbols = awgn(modulated, snr=snr)
+        llr = dem(noise_symbols, variance=awgn.variance)
+        decoded = pc.decode(llr)
+        err_sum[i] += np.sum(np.bitwise_xor(bits, decoded))
 
-print(err_sum / (k * frames))
+print(err_sum / frames)

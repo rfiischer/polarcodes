128d7070e9343969403672fadec6b3c378d8d699
Rodrigo Fischer <rodrfischer@hotmail.com>
2020-03-17 20:43:07 -0300

    [encoder_decoder_awgn refactor]

---
+++ scripts/verifications/encoder_decoder_awgn/encoder_decoder_awgn.py
@@ -0,0 +1,46 @@
+"""
+
+
+Created on 17/02/2020 17:33
+
+@author: Rodrigo Fischer (rodrigoarfischer@gmail.com)
+"""
+
+import numpy as np
+from time import time, strftime, gmtime
+
+from coding.polarcoding import PolarCoding
+from core.utils.awgn import AWGN
+from core.utils.constellation import PolarConstellation
+from core.utils.mod_demod import Modulator, Demodulator
+
+n = 4
+k = 1
+frames = 1000000
+snr = 1 + 10 * np.log10(0.5)
+rng = np.random.RandomState(seed=12465)
+awgn = AWGN(1, rng, snr_unit="EsN0_dB")
+constellation = PolarConstellation()
+mod = Modulator(constellation, 1)
+dem = Demodulator(constellation, 'llr_exact', 1)
+
+start_time = time()
+err_sum = np.zeros(2 ** n)
+for i in range(0, 2 ** n):
+    print('{}\n'.format(i))
+    for frame in range(0, frames):
+        rel = np.concatenate(([i], np.delete(np.arange(0, 16), i)))
+        pc = PolarCoding(n, rel_idx=rel)
+        bits = np.random.randint(0, 2, k, dtype=np.uint8)
+        encoded = pc.encode(bits)
+        modulated = mod(encoded)
+        noise_symbols = awgn(modulated, snr=snr)
+        llr = dem(noise_symbols, variance=awgn.variance)
+        decoded = pc.decode(llr)
+        err_sum[i] += np.sum(np.bitwise_xor(bits, decoded))
+
+tot_time = time() - start_time
+print("Total time: {}".format(strftime('%Hhr:%Mmin:%Ssec', gmtime(tot_time))))
+
+print(err_sum / frames)
+np.savetxt('results.txt', err_sum / frames)
---
+++ scripts/verifications/encoder_decoder_awgn/plot.py
@@ -0,0 +1,29 @@
+"""
+
+Created on 17/03/2020 15:12
+
+@author: Rodrigo Fischer (rodrigoarfischer@gmail.com)
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from coding.polarcoding.construction import tahir
+
+data = np.loadtxt('results.txt')
+
+marker_style_0 = dict(color='tab:blue', linestyle='', marker='o',
+                      markersize=10, markerfacecoloralt='tab:red')
+
+marker_style_1 = dict(color='tab:red', linestyle='', marker='*',
+                      markersize=10, markerfacecoloralt='tab:red')
+
+plt.semilogy(data, fillstyle='none', **marker_style_0)
+plt.semilogy(tahir(4, -2)[1], fillstyle='none', **marker_style_1)
+plt.ylim(1e-6, 1)
+plt.ylabel("BER")
+plt.xlabel("Bit Channel")
+plt.xticks(np.arange(0, 16))
+plt.grid(True, which="both")
+
+plt.show()

d77753330dd2284eefb8dadf9c7ebe355a244f8e
Rodrigo Fischer <rodrfischer@hotmail.com>
2020-05-06 13:10:28 -0300

    [num_workers_campaign plot refinment]

--- scripts/campaigns/num_workers_campaign/plot.py
+++ scripts/campaigns/num_workers_campaign/plot.py
@@ -12,15 +12,17 @@ import re
 import glob
 
 
-def plot_ber(graphs, legends, title, linestyle, xlabel=r'$E_b/N_0$ (dB)', ylabel='BER', ylim=None, xlim=None):
+def plot_ber(graphs, legends, title, linestyle, xlabel=r'$E_b/N_0$ (dB)', ylabel='BER', ylim=None, xlim=None,
+             fontsize=10):
     fig, ax = plt.subplots()
     for i, graph in enumerate(graphs):
-        ax.semilogy(graph[0], graph[1], linestyle[i], fillstyle='none', markersize=8)
+        ax.semilogy(graph[0], graph[1], linestyle[i], fillstyle='none', markersize=8, subsy=[2, 5, 8])
 
     ax.grid(which='both')
-    ax.legend(legends)
-    ax.set_xlabel(xlabel)
-    ax.set_ylabel(ylabel)
+    ax.legend(legends, fontsize=fontsize)
+    ax.set_xlabel(xlabel, fontsize=fontsize)
+    h = ax.set_ylabel(ylabel, fontsize=fontsize)
+    # h.set_rotation(0)
     ax.set_title(r"{} for ".format(ylabel) + title)
 
     if ylim is not None:
@@ -30,6 +32,8 @@ def plot_ber(graphs, legends, title, linestyle, xlabel=r'$E_b/N_0$ (dB)', ylabel
         ax.set_xlim(xlim)
 
     fig.tight_layout()
+
+    return fig, ax
 
 
 # Change plot font
@@ -43,7 +47,11 @@ for subdir in glob.glob('results/**/'):
     method_name = subdir.split('\\')[-2]
     workers = int(re.split('(\d+)', method_name)[-2])
     num_workers.append(workers)
-    methods.append('{} Workers'.format(workers))
+    if workers > 1:
+        plural = 's'
+    else:
+        plural = ''
+    methods.append('{} Worker{}'.format(workers, plural))
 
     ber = np.loadtxt('results//{}//ber.txt'.format(method_name))[:, 1]
     ber_range = np.loadtxt('results//{}//ber.txt'.format(method_name))[:, 0]
@@ -54,19 +62,24 @@ for subdir in glob.glob('results/**/'):
     plot_list.append([ber_range, ber])
 
 # QPSK plot - EbN0
-methods = sorted(num_workers)
+methods = [i[0] for i in sorted(zip(methods, num_workers), key=lambda item: item[1])]
 plot_list = [i[0] for i in sorted(zip(plot_list, num_workers), key=lambda item: item[1])]
 
 desired_curves = [0, 4, 8]
 methods = [methods[i] for i in desired_curves]
 plot_list = [plot_list[i] for i in desired_curves]
 
-plot_ber(plot_list,
-         tuple(methods),
-         "",
-         ['g--o', 'r-.^', 'b:*'],
-         ylim=[1e-7, 1],
-         xlim=[-3.2, 0.2],
-         xlabel=r'$E_s/N_0$ (dB)')
+fig, ax = plot_ber(plot_list,
+                   tuple(methods),
+                   "Polar Coding",
+                   ['g--o', 'r-.^', 'b:*'],
+                   ylim=[1e-7, 1],
+                   xlim=[-3.2, 0.2],
+                   xlabel=r'$E_s/N_0$ (dB)',
+                   fontsize=13)
+
+plt.grid(True, which="minor", ls="--")
+ax.set_yticklabels(['1e-{}'.format(i) for i in range(8, -1, -1)])
+# ax.yaxis.set_label_coords(-0.05,1.02)
 
 plt.show()

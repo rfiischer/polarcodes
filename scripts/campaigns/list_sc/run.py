e9c0036d34a625a5201853e67688a102ade06f95
Rodrigo Fischer <rodrfischer@hotmail.com>
2020-08-17 15:50:50 -0300

    [list-sc campaign refactor]

--- scripts/campaigns/list_sc/run.py
+++ scripts/campaigns/list_sc/run.py
@@ -11,22 +11,22 @@ from tcc.main import main
 if __name__ == "__main__":
     list_sizes = [1, 2, 4, 8, 16, 32]
     for list_size in list_sizes:
-        main("parameters/batch_0.ini", {'POLAR': {'construction_method': '"{}"'.format('bhattacharyya'),
+        main("parameters/batch_0.ini", {'POLAR': {'construction_method': '"{}"'.format('tahir'),
                                                   'list_size': list_size,
                                                   'encoding_mode': '"systematic"'},
                                         'GENERAL': {'log_file': '"log/{}_log.log"'.format('s_l' +
                                                                                           str(list_size)),
                                                     'results_dir': '"results/{}/"'.format('s_l' +
                                                                                           str(list_size))},
-                                        'RUN': {'num_workers': 4,
-                                                'frame_pack_size': 4}})
+                                        'RUN': {'num_workers': 16,
+                                                'frame_pack_size': 16}})
 
-        main("parameters/batch_0.ini", {'POLAR': {'construction_method': '"{}"'.format('bhattacharyya'),
+        main("parameters/batch_0.ini", {'POLAR': {'construction_method': '"{}"'.format('tahir'),
                                                   'list_size': list_size,
                                                   'encoding_mode': '"non-systematic"'},
                                         'GENERAL': {'log_file': '"log/{}_log.log"'.format('n_l' +
                                                                                           str(list_size)),
                                                     'results_dir': '"results/{}/"'.format('n_l' +
                                                                                           str(list_size))},
-                                        'RUN': {'num_workers': 4,
-                                                'frame_pack_size': 4}})
+                                        'RUN': {'num_workers': 16,
+                                                'frame_pack_size': 16}})

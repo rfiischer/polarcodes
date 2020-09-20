2021f7856931b81c494371270c22c3044fe7ad51
Rodrigo Fischer <rodrfischer@hotmail.com>
2020-09-20 14:19:06 -0300

    [list_decoder_characterization refactor]

--- scripts/thesis_campaigns/list_decoder_characterization/run.py
+++ scripts/thesis_campaigns/list_decoder_characterization/run.py
@@ -9,39 +9,9 @@ Created on 10/03/2020 20:58
 from tcc.main import main
 
 if __name__ == "__main__":
-    main("parameters/batch_0.ini", {'POLAR': {'construction_method': '"{}"'.format('tahir'),
-                                              'encoding_mode': '"non-systematic"',
-                                              'decoding_algorithm': '"ssc"'},
-                                    'GENERAL': {'log_file': '"log/{}_log.log"'.format('curve_1'),
-                                                'results_dir': '"results/{}/"'.format('curve_1')},
-                                    'RUN': {'num_workers': 16,
-                                            'frame_pack_size': 16}})
-
-    main("parameters/batch_0.ini", {'POLAR': {'construction_method': '"{}"'.format('tahir'),
-                                              'encoding_mode': '"non-systematic"',
-                                              'decoding_algorithm': '"sscl-spc"',
-                                              'list_size': 32},
-                                    'GENERAL': {'log_file': '"log/{}_log.log"'.format('curve_2'),
-                                                'results_dir': '"results/{}/"'.format('curve_2')},
-                                    'RUN': {'num_workers': 16,
-                                            'frame_pack_size': 16}})
-
-    main("parameters/batch_0.ini", {'POLAR': {'construction_method': '"{}"'.format('tahir'),
-                                              'encoding_mode': '"non-systematic"',
-                                              'decoding_algorithm': '"sscl-spc-crc"',
-                                              'list_size': 32,
-                                              'crc_id': '"crc-16"'},
-                                    'GENERAL': {'log_file': '"log/{}_log.log"'.format('curve_3'),
-                                                'results_dir': '"results/{}/"'.format('curve_3')},
-                                    'RUN': {'num_workers': 16,
-                                            'frame_pack_size': 16}})
-
-    main("parameters/batch_0.ini", {'POLAR': {'construction_method': '"{}"'.format('tahir'),
-                                              'encoding_mode': '"systematic"',
-                                              'decoding_algorithm': '"sscl-spc-crc"',
-                                              'list_size': 32,
-                                              'crc_id': '"crc-16"'},
-                                    'GENERAL': {'log_file': '"log/{}_log.log"'.format('curve_4'),
-                                                'results_dir': '"results/{}/"'.format('curve_4')},
-                                    'RUN': {'num_workers': 16,
-                                            'frame_pack_size': 16}})
+    for list_size in [1, 2, 4, 8, 16, 32]:
+        main("parameters/batch_0.ini", {'POLAR': {'list_size': list_size},
+                                        'GENERAL': {'log_file': '"log/{}_log.log"'.format('l_' + str(list_size)),
+                                                    'results_dir': '"results/{}/"'.format('l_' + str(list_size))},
+                                        'RUN': {'num_workers': 16,
+                                                'frame_pack_size': 16}})

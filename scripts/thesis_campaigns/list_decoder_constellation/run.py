ea4ecce6ec4ab830c97ff3cfc84b12a244963e9d
Rodrigo Fischer <rodrfischer@hotmail.com>
2020-11-11 15:27:11 -0300

    [small changes in constellation campaign]

--- scripts/thesis_campaigns/list_decoder_constellation/run.py
+++ scripts/thesis_campaigns/list_decoder_constellation/run.py
@@ -9,17 +9,35 @@ Created on 10/03/2020 20:58
 from tcc.main import main
 
 if __name__ == "__main__":
-    for list_size in [1, 8]:
-        main("parameters/batch_0.ini", {'POLAR': {'list_size': list_size,
-                                                  'construction_method': '"dega"'},
-                                        'GENERAL': {'log_file': '"log/{}_log.log"'.format('ldega_' + str(list_size)),
-                                                    'results_dir': '"results/{}/"'.format('ldega_' + str(list_size))},
-                                        'RUN': {'num_workers': 16,
-                                                'frame_pack_size': 16}})
 
-        main("parameters/batch_0.ini", {'POLAR': {'list_size': list_size,
-                                                  'construction_method': '"mdega"'},
-                                        'GENERAL': {'log_file': '"log/{}_log.log"'.format('lmdega_' + str(list_size)),
-                                                    'results_dir': '"results/{}/"'.format('lmdega_' + str(list_size))},
-                                        'RUN': {'num_workers': 16,
-                                                'frame_pack_size': 16}})
+    main("parameters/batch_0.ini", {'POLAR': {'list_size': 8,
+                                              'construction_method': '"dega"',
+                                              'decoding_algorithm': '"sscl-spc-crc"',
+                                              'crc_id': '"crc-16"'},
+                                    'GENERAL': {'log_file': '"log/{}_log.log"'.format('l_dega_8'),
+                                                'results_dir': '"results/{}/"'.format('l_dega_8')},
+                                    'RUN': {'num_workers': 16,
+                                            'frame_pack_size': 16}})
+
+    main("parameters/batch_0.ini", {'POLAR': {'list_size': 8,
+                                              'construction_method': '"mdega"',
+                                              'decoding_algorithm': '"sscl-spc-crc"',
+                                              'crc_id': '"crc-16"'},
+                                    'GENERAL': {'log_file': '"log/{}_log.log"'.format('l_mdega_8'),
+                                                'results_dir': '"results/{}/"'.format('l_mdega_8')},
+                                    'RUN': {'num_workers': 16,
+                                            'frame_pack_size': 16}})
+
+    main("parameters/batch_0.ini", {'POLAR': {'construction_method': '"dega"',
+                                              'decoding_algorithm': '"fast-ssc"'},
+                                    'GENERAL': {'log_file': '"log/{}_log.log"'.format('f_dega'),
+                                                'results_dir': '"results/{}/"'.format('f_dega')},
+                                    'RUN': {'num_workers': 16,
+                                            'frame_pack_size': 16}})
+
+    main("parameters/batch_0.ini", {'POLAR': {'construction_method': '"mdega"',
+                                              'decoding_algorithm': '"fast-ssc"'},
+                                    'GENERAL': {'log_file': '"log/{}_log.log"'.format('f_mdega'),
+                                                'results_dir': '"results/{}/"'.format('f_mdega')},
+                                    'RUN': {'num_workers': 16,
+                                            'frame_pack_size': 16}})

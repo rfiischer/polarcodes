0495ba775b1cd96a388111a647ba9a38b784b079
Rodrigo Fischer <rodrfischer@hotmail.com>
2020-11-24 20:56:00 -0300

    [snr_design plot refactor]

--- scripts/thesis_campaigns/snr_design/plot.py
+++ scripts/thesis_campaigns/snr_design/plot.py
@@ -18,23 +18,31 @@ plot_list = []
 ber_vals = []
 snr_vals = []
 
+fer_vals = []
+
 for subdir in glob.glob('results/**/'):
     method_name = subdir.split('\\')[-2]
     snr = float(method_name.split('_')[-1])
 
     _, ber = np.loadtxt('results//{}//ber.txt'.format(method_name))
+    _, fer = np.loadtxt('results//{}//fer.txt'.format(method_name))
 
     ber_vals.append(ber)
+    fer_vals.append(fer)
     snr_vals.append(snr)
 
 ber_data = np.zeros((len(ber_vals), 2))
+fer_data = np.zeros((len(fer_vals), 2))
 ber_data[:, 0] = snr_vals
 ber_data[:, 1] = ber_vals
+fer_data[:, 0] = snr_vals
+fer_data[:, 1] = fer_vals
 
 if not os.path.exists('results//condensed'):
     os.makedirs('results//condensed')
 
 np.savetxt('results//condensed//ber.txt', ber_data)
+np.savetxt('results//condensed//fer.txt', fer_data)
 
 # QPSK plot - EbN0
 thickness = [2]
